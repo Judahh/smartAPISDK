019a4b6a586bb46a9838a8e36724ecd0e5cf106c
Judahh <judahholanda7@gmail.com>
2023-06-05 16:07:38 -0300

    add request api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Rest, PathTree, InputTypeTree, TypeTree } from './rest/rest';
-import { request } from './rest/request';
+import { RequestAPI, request } from './rest/request';
 import RequestError from './rest/requestError';
 
 export { Rest, request, RequestError };
-export type { PathTree, InputTypeTree, TypeTree };
+export type { PathTree, InputTypeTree, TypeTree, RequestAPI };
