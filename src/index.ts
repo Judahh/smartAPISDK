6fb402e2057fa3894e652ebe5b6666236c87f3ce
Judahh <judahholanda7@gmail.com>
2023-06-05 16:22:36 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { Rest, PathTree, InputTypeTree, TypeTree } from './rest/rest';
 import { RequestAPI, request } from './rest/request';
 import RequestError from './rest/requestError';
 
-export { Rest, request, RequestError };
-export type { PathTree, InputTypeTree, TypeTree, RequestAPI };
+export { Rest, request, RequestError, RequestAPI };
+export type { PathTree, InputTypeTree, TypeTree };
